50a99be01dc7abd9002d1a8803c596a13cd4aed1
JomariHinayon <hinayonjomari@gmail.com>
2022-08-13 12:44:51 +0800

    add animation in every page section

--- js/animation.js
+++ js/animation.js
@@ -2,19 +2,78 @@
 $(document).ready(function () {
 
 
+    $(".nav-item").show("slide", {
+        direction: "up"
+    }, 1200);
+
+
+    $(".cartoon").show("slide", {
+        direction: "right"
+    }, 1200);
+
+    $(".intro-resume").slideDown(1500);
+
+
 // animation if scrolled
 
 $(window).scroll(function(event){
-   var top = $(this).scrollTop();
-   if (top > 0){
+   var scrolledTop = $(this).scrollTop();
+   var scrolledBottom = $(this).scrollTop() + $(this).height();
+
+   var positionAbout = $("#section-about").offset().top + $("#section-about").width() / 2 - 300;
+   var positionProjects = $("#section-portfolio").offset().top + $("#section-portfolio").width() / 2 - 200;
+   var positionSkills = $("#section-skills").offset().top + $("#section-skills").width() / 2 - 300;
+   var positionContact = $("#section-contact").offset().top + $("#section-contact").width() / 2 - 350;
+
+   if (scrolledTop > 0){
       $(".navbar").animate({
         backgroundColor: "#e8e8e8",
         boxShadow: "0 2px 4px 4px rgba(0,0,0,1)"
-      }, 500);
+      }, 500).addClass("fixed-top");
+
+    //   console.log("Position scroll: "+ scrolledBottom);
+    //   console.log("Position: "+ positionContact);
+
+
+    //   if the scrollebar bottom reach the about
+    if(scrolledBottom >= positionAbout){
+        $(".my-pic-container").show("slide", {
+            direction: "down"
+        }, 1200);
+       $(".about-me-text").show("slide", {
+            direction: "up"
+        }, 1200); 
+    }
+
+     //   if the scrollebar bottom reach the projects section
+    if(scrolledBottom >= positionProjects){
+        $(".carousel").show("slide", {
+            direction: "right"
+        }, 1000);    
+    }
+
+     //   if the scrollebar bottom reach the projects section
+    if(scrolledBottom >= positionSkills){
+        $(".container-skill").show("slide", {
+            direction: "down"
+        }, 1000);    
+    }
+
+    //   if the scrollebar bottom reach the projects section
+    if(scrolledBottom >= positionContact){
+        $("#section-contact a").show("slide", {
+            direction: "up"
+        }, 1000);    
+    }
+
+
+
+
+
    } else {
      $(".navbar").animate({
         backgroundColor: "transparent"
-      }, 500);
+      }, 1000);
    }
 });
 
--- js/typed/type-writer-effect.js
+++ js/typed/type-writer-effect.js
@@ -4,5 +4,8 @@ var typing = new Typed(".intro-name", {
   strings: ["Hello!", "I'm Jomari Hinayon"],
   typeSpeed: 150,
   backSpeed: 150,
-  loop: true
+  loop: true,
+  startDelay: 1100
 });
+
+// console.log(typing)
